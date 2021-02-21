52be2f101469b8617e7ef7f2df2c9d53d17dd5e9
Tiewly <tananporn2112@gmail.com>
2021-02-21 22:53:52 +0700

    Edit and decorate

--- pages/timeline.vue
+++ pages/timeline.vue
@@ -11,78 +11,124 @@
         </h2></v-card-title
       >
     </v-img>
-    <v-text-field
-      v-model="search"
-      label="วันที่"
-      append-icon="mdi-magnify"
-      outlined
-      hide-details
-      dense
-      style="padding: 5% 5% 3% 5%"
-    />
-    <v-data-table
-      :headers="headers"
-      :items="workHourFormated"
-      :items-per-page="5"
-      :search="search"
-      mobile-breakpoint
-    >
-      <template v-slot:item.in="{ item }">
-        <v-chip :color="getColor(item.inErr)" dark>
-          {{ item.inDate }}
-        </v-chip>
-      </template>
-      <template v-slot:item.out="{ item }">
-        <v-chip :color="getColor(item.outErr)" dark>
-          {{ item.outDate }}
-        </v-chip>
-      </template>
-    </v-data-table>
+    <v-row>
+      <v-col style="margin-left: 3%; margin-right: 3%">
+        <br />
+        <v-row>
+          <v-col style="max-width: 180px"
+            ><v-text-field
+              v-model="start"
+              type="date"
+              label="จากวันที่"
+              dense
+              outlined
+              hide-details
+            ></v-text-field
+          ></v-col>
+          <v-col style="max-width: 180px"
+            ><v-text-field
+              v-model="end"
+              type="date"
+              label="ถึงวันที่"
+              dense
+              outlined
+              hide-details
+            ></v-text-field
+          ></v-col>
+          <v-btn
+            block
+            color="info"
+            @click="$emit('getWorkHour', { start, end })"
+            >ค้นหา</v-btn
+          >
+        </v-row>
+        <br />
+        <v-data-table
+          :headers="headers"
+          :items="workHourFormated"
+          :items-per-page="5"
+          mobile-breakpoint
+        >
+          <!-- <template v-slot:item.in="{ item }">
+            <v-btn
+              v-if="item.in"
+              :color="getColor(item.inErr)"
+              dark
+              @click="
+                showMap = true
+                goTo(item.outAddr)
+              "
+              >{{ item.inDate }}</v-btn
+            >
+          </template>
+          <template v-slot:item.out="{ item }">
+            <v-btn
+              v-if="item.out"
+              :color="getColor(item.outErr)"
+              dark
+              @click="
+                showMap = true
+                goTo(item.inAddr)
+              "
+              >{{ item.outDate }}</v-btn
+            >
+          </template> -->
+          <template v-slot:item.sum="{ item }">
+            <v-btn v-if="item.leave" color="info" dark>{{ item.sum }}</v-btn>
+            <span v-else>{{ item.sum }}</span>
+          </template>
+        </v-data-table>
+      </v-col>
+      <!-- <v-col :cols="showMap ? 4 : 0" v-show="showMap">
+        <gmap-map
+          ref="mapRef"
+          :center="{ lat: 18.7957392, lng: 98.9526686 }"
+          :zoom="18"
+          style="width: 100%; height: 70vh"
+        >
+          <gmap-marker :position="marker" :draggable="false" />
+        </gmap-map>
+      </v-col> -->
+    </v-row>
   </v-card>
 </template>
 
 <script>
 export default {
+  props: {
+    workHour: {
+      type: Array,
+      default: () => [],
+    },
+  },
   data: () => ({
-    search: '',
+    showMap: false,
+    start: '',
+    end: '',
     headers: [
-      { text: 'วัน/เดือน/ปี', value: 'date', align: 'center' },
-      { text: 'เวลาเข้า', value: 'in', align: 'center' },
-      { text: 'เวลาออก', value: 'out', align: 'center' },
-      { text: 'รวม (hr:min)', value: 'sum', align: 'center' },
+      { text: 'เดือน/วัน/ปี', value: 'date', align: 'center' },
+      { text: 'เวลาเข้า', value: 'in', align: 'center', sortable: false },
+      { text: 'เวลาออก', value: 'out', align: 'center', sortable: false },
+      { text: 'รวม (hr:min)', value: 'sum', align: 'center', sortable: false },
     ],
-    workHour: [
-      {
-        date: '01/10/2020',
-        amount: 8,
-        attendance: [
-          {
-            time: 'Sat Jan 23 2021 08:33:35 GMT+0700 (Indochina Time)',
-            errorTime: 18,
-            type: 1,
-          },
-          {
-            time: 'Sat Jan 23 2021 17:33:35 GMT+0700 (Indochina Time)',
-            errorTime: -1,
-            type: 2,
-          },
-        ],
-      },
-    ],
+    //map
+    marker: { lat: 0, lng: 0 },
   }),
   computed: {
     workHourFormated() {
       const whs = this.workHour
       var r = []
+      var i = -1
       whs.forEach((wh) => {
-        var i = -1
         wh.attendance.forEach((att) => {
-          if (att.type == 1) {
+          console.log(this.dateFormated(new Date(att.time)) + '/' + att.type)
+          if (att.type === '1') {
+            i++
             r.push({
               date: wh.date,
               in: new Date(att.time),
               inDate: this.dateFormated(new Date(att.time)),
-              inErr: att.errorTime,
+              inErr: att.isLate,
               inAddr: att.location,
               out: '',
               outDate: '',
@@ -90,36 +136,46 @@ export default {
               outAddr: '',
               sum: '',
             })
-            i++
-          } else {
+          } else if (att.type === '2') {
             r[i].out = new Date(att.time)
             ;(r[i].outDate = this.dateFormated(new Date(att.time))),
-              (r[i].outErr = att.errorTime)
+              (r[i].outErr = att.isLate)
             r[i].outAddr = att.location
             var sum = (r[i].out - r[i].in) / 1000 / 60
             var h = this.shiftZero(sum / 60)
             var m = this.shiftZero(sum % 60)
             r[i].sum = h + ':' + m
+          } else if (att.type === '3') {
+            i++
+            r.push({
+              date: wh.date,
+              sum: 'ลา',
+              leave: true,
+            })
           }
         })
+        console.log(JSON.stringify(r[i]) + ' :' + i)
       })
       return r
     },
   },
   methods: {
-    getColor(i) {
-      if (i > 0) return 'success'
-      else return 'warning'
-    },
+    getColor: (late) => (late ? 'warning' : 'success'),
     shiftZero(m) {
-      if (m >= 10) return `${m}`
-      else if (m > 0) return `0${m}`
-      else return '00'
+      var res
+      m >= 10 ? (res = `${m}`) : m > 0 ? (res = `0${m}`) : (res = '00')
+      return res
     },
     dateFormated(d) {
       var m = this.shiftZero(d.getMinutes())
       var h = this.shiftZero(d.getHours())
       return `${h}:${m}`
+    },
+    goTo(addr) {
+      this.$refs.mapRef.$mapPromise.then((map) => {
+        map.panTo(addr)
+      })
+      this.marker = addr
     },
   },
 }
