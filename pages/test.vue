d559b2b9b0792ef703b28e7b3b72aa7a7c26afe1
Tiewly <tananporn2112@gmail.com>
2021-03-03 01:21:09 +0700

    Test for friend

--- pages/test.vue
+++ pages/test.vue
@@ -1,20 +1,24 @@
 <template>
-<div>
-  <DateTimeInput
-    header="Start Date Time"
-    @onTimeChange="t => time = t"
-    @onDateChange="d => date = d"
-  />
-  <h1>this is out of component</h1>
-  <p>date: {{date}}</p>
-  <p>time: {{time}}</p>
-</div>
+  <div>
+    <a v-if="!inProgess" @click="startOrder">start</a>
+    <a v-else @click="finishOrder">finish</a>
+  </div>
 </template>
 <script>
 export default {
   data: () => ({
-    date: null,
-    time: null,
-  })
+    inProgess: false,
+    //...
+  }),
+  methods: {
+    startOrder() {
+      this.inProgess = true
+      //...
+    },
+    finishOrder() {
+      this.inProgess = false
+      //...
+    },
+  },
 }
 </script>
